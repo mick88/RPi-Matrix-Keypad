9e8f8649c23f513105a0589ee74bee17752c8006
michaldabski <contact@michaldabski.com>
2016-08-03 21:36:59 +0100

    More cleanup and implement context manager

--- matrix_keypad.py
+++ matrix_keypad.py
@@ -36,43 +36,35 @@ class Keypad(object):
             GPIO.remove_event_detect(self.COLUMN[c])
 
         # get column number
-        colVal = -1
-        for c in range(len(self.COLUMN)):
-            if channel == self.COLUMN[c]:
-                colVal = c
+        col_val = self.COLUMN.index(channel)
 
         # continue if valid column (it should always be)
-        if colVal >=0 and colVal < len(self.COLUMN):
+        if 0 <= col_val < len(self.COLUMN):
 
             # set rows as inputs
-            for r in range(len(self.ROW)):
-                GPIO.setup(self.ROW[r], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            for r in self.ROW:
+                GPIO.setup(r, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-            #set triggered column as low output
+            # set triggered column as low output
             GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
 
             # Scan rows for pushed key/button
             row_val = -1
-            for r in range(len(self.ROW)):
-                tmpRead = GPIO.input(self.ROW[r])
-                if tmpRead == 0:
+            for r in self.ROW:
+                if GPIO.input(r) == 0:
                     row_val = r
                     break
 
-            #continue if row is valid (possible that it might not be if the key was very quickly released)
-            if row_val >= 0 and row_val < len(self.ROW):
+            # continue if row is valid (possible that it might not be if the key was very quickly released)
+            if 0 <= row_val < len(self.ROW):
                 # send key info right away
-                self._callback(self.KEYPAD[row_val][colVal])
+                self._callback(self.KEYPAD[row_val][col_val])
                 # This avoids nasty bouncing errors when the key is released
                 # By waiting for the rising edge before re-enabling interrupts it
                 # avoids interrupts fired due to bouncing on key release and
                 # any repeated interrupts that would otherwise fire.
-                try:
-                    GPIO.wait_for_edge(self.ROW[row_val], GPIO.RISING)
-                    self._set_interrupt_mode()
-                except RuntimeError:
-                    pass
-
+                GPIO.wait_for_edge(self.ROW[row_val], GPIO.RISING)
+                self._set_interrupt_mode()
                 return
 
             else:
@@ -100,23 +92,25 @@ class Keypad(object):
             GPIO.setup(self.ROW[r], GPIO.OUT, initial=GPIO.LOW)
 
         # set columns as inputs and attach interrupt handlers on rising edge
-        for c in range(len(self.COLUMN)):
-            GPIO.setup(self.COLUMN[c], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(self.COLUMN[c], GPIO.FALLING, bouncetime=250, callback=self.__changeWrapper)
+        for c in self.COLUMN:
+            GPIO.setup(c, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(c, GPIO.FALLING, bouncetime=250, callback=self.__changeWrapper)
 
     def cleanup(self):
         GPIO.cleanup()
         print "Cleanup done!"
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cleanup()
+
+
 if __name__ == '__main__':
-    def keypadCallback(value):
+    def callback(value):
         print "Keypad: " + str(value)
 
-    key = Keypad(keypadCallback)
-
-    try:
+    with Keypad(callback):
         while True:
             time.sleep(1)
-
-    except KeyboardInterrupt:
-        key.cleanup()
